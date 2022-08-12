93353cb775eee1211d1410a2096b00553571c91e
Agrit Tiwari <agrit1408@gmail.com>
2022-08-12 21:47:37 +0530

    basic setup for firebase react-native paper and flashlist

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlashList } from "@shopify/flash-list";
+
 
 export default function App() {
+  const DATA = [
+    {
+      title: "First Item",
+    },
+    {
+      title: "Second Item",
+    },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+      <Text>Okay
+         
+      </Text>
+      <FlashList
+      data={DATA}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={20}
+    />
       <StatusBar style="auto" />
     </View>
   );
